59efeb3b941dabec9080d2f3a0e303bf02961714
Shubham <shubho.193@gmail.com>
2017-07-03 15:50:41 +0530

    Fixed some bugs

--- app/src/main/java/com/example/shubham/googlesignindemo/MainActivity.java
+++ app/src/main/java/com/example/shubham/googlesignindemo/MainActivity.java
@@ -26,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     EditText editText_email,editText_pass;
     Activity activity=this;
 
+    String emailID,password;
+    FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +38,8 @@ public class MainActivity extends AppCompatActivity {
         editText_email=(EditText)findViewById(R.id.email);
         editText_pass=(EditText)findViewById(R.id.password);
 
-        final String emailID,password;
-        emailID=editText_email.getText().toString();
-        password=editText_pass.getText().toString();
+
+
 
 
         firebaseAuth=FirebaseAuth.getInstance();
@@ -49,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
-                FirebaseUser user=firebaseAuth.getCurrentUser();
+                user=firebaseAuth.getCurrentUser();
                 if(user!=null)
                     Log.d("NewState","onAuthStateChanged: signed_In"+ user.getDisplayName());
                 else
@@ -62,29 +64,58 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (emailID.equals("") && password.equals("") && Patterns.EMAIL_ADDRESS.matcher(emailID).matches()) {
 
-                   firebaseAuth.createUserWithEmailAndPassword(emailID,password)
-                           .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
-                               @Override
-                               public void onComplete(@NonNull Task<AuthResult> task) {
+                emailID=editText_email.getText().toString();
+                password=editText_pass.getText().toString();
+                if ((!emailID.equals("")) && (!password.equals("")) && Patterns.EMAIL_ADDRESS.matcher(emailID).matches()) {
 
-                                   Log.d("Success", "createUserWithEmail:onComplete:" + task.isSuccessful());
+//                    Toast.makeText(activity, "Right UID/Password", Toast.LENGTH_SHORT).show();
+//                   firebaseAuth.createUserWithEmailAndPassword(emailID,password)
+//                           .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+//                               @Override
+//                               public void onComplete(@NonNull Task<AuthResult> task) {
+//
+//                                   Log.d("Success", "createUserWithEmail:onComplete:" + task.isSuccessful());
+//
+//                                   // If sign in fails, display a message to the user. If sign in succeeds
+//                                   // the auth state listener will be notified and logic to handle the
+//                                   // signed in user can be handled in the listener.
+//                                   if (!task.isSuccessful()) {
+//                                       Toast.makeText(activity, "auth failed",
+//                                               Toast.LENGTH_SHORT).show();
+//                                   }
+//                               }
+//                           });
+                 firebaseAuth.signInWithEmailAndPassword(emailID,password)
+                         .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                             @Override
+                             public void onComplete(@NonNull Task<AuthResult> task) {
 
-                                   // If sign in fails, display a message to the user. If sign in succeeds
-                                   // the auth state listener will be notified and logic to handle the
-                                   // signed in user can be handled in the listener.
-                                   if (!task.isSuccessful()) {
-                                       Toast.makeText(activity, "auth failed",
-                                               Toast.LENGTH_SHORT).show();
-                                   }
-                               }
-                           });
+
+                                 user=firebaseAuth.getCurrentUser();
+
+                                 Log.d("Success", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+
+
+                                 // If sign in fails, display a message to the user. If sign in succeeds
+                                 // the auth state listener will be notified and logic to handle the
+                                 // signed in user can be handled in the listener.
+                                 if (!task.isSuccessful()) {
+                                     Log.d("Fail", "signInWithEmail:failed", task.getException());
+                                     Toast.makeText(activity, "FAILED",
+                                             Toast.LENGTH_SHORT).show();
+                                 }
+                             }
+                         }) ;
                 }
+                else
+                    Toast.makeText(activity, "Wrong UID/Password", Toast.LENGTH_SHORT).show();
             }
 
         });
     }
+
 
     @Override
     protected void onStart() {
@@ -97,7 +128,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         //Removing the listener object
-        firebaseAuth.removeAuthStateListener(authStateListener);
+        if (authStateListener != null)
+            firebaseAuth.removeAuthStateListener(authStateListener);
 
     }
 }
